73ae3b6088c681448c4983317f974d74e015c355
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-03-29 11:06:16 +0530

    solutions

---
+++ Problem Set/Chapter 7/p2.py
@@ -0,0 +1,4 @@
+l=["harry","Sarry","carry","marry"]
+for name in l:
+    if(name.startswith("S")):
+        print("Hello "+name)
---
+++ Problem Set/Chapter 7/p6.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number: "))
+
+product=1
+
+for i in range(1,n+1):
+
+    product=product*i
+
+print(f"Factorail of {n} is {product}")
